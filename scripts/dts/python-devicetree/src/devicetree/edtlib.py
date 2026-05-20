26b02333779467620f660e13af94ec9a1a220a3d
Fin Maaß <f.maass@vogl-electronic.com>
2026-05-20 10:06:26 +0200

    dts: add dependency-mode prop key
    add dependency-mode prop key, so it can be
    changed what dependency a phandle
    in the dt introduces. With this it can be set
    in the dt binding that a phandle is f.e. ignored
    when the dt ordinal is calculated.
    
    Signed-off-by: Fin Maaß <f.maass@vogl-electronic.com>

--- scripts/dts/python-devicetree/src/devicetree/edtlib.py
+++ scripts/dts/python-devicetree/src/devicetree/edtlib.py
@@ -495,7 +495,7 @@ class Binding:
 
         ok_prop_keys = {"description", "type", "required",
                         "enum", "const", "default", "deprecated",
-                        "specifier-space", "min", "max"}
+                        "specifier-space", "min", "max", "dependency-mode"}
 
         for prop_name, options in raw["properties"].items():
             for key in options:
@@ -593,6 +593,10 @@ class PropertySpec:
     max:
       The maximum value the property may take as given in the binding, or None.
       Only applicable to 'int' and 'array' type properties.
+
+    dependency_mode:
+      Specifies how the dependency graph should handle this property.
+      Can be "ignore", "child-ignore", "reverse", "normal" or None.
     """
 
     def __init__(self, name: str, binding: Binding):
@@ -673,6 +677,11 @@ class PropertySpec:
     def deprecated(self) -> bool:
         "See the class docstring"
         return self._raw.get("deprecated", False)
+
+    @property
+    def dependency_mode(self) -> Optional[str]:
+        "See the class docstring"
+        return self._raw.get("dependency-mode")
 
     @property
     def specifier_space(self) -> Optional[str]:
@@ -2444,6 +2453,26 @@ class EDT:
         except Exception as e:
             raise EDTError(e) from None
 
+    def _apply_dependency_mode(self, root_node: Node, dep_node: Node, prop: Property) -> None:
+        match prop.spec.dependency_mode:
+            case None | "normal":
+                self._graph.add_edge(root_node, dep_node)
+            case "reverse":
+                self._graph.add_edge(dep_node, root_node)
+            case "ignore":
+                pass
+            case "child-ignore":
+                def _is_child(child_node: Optional[Node]) -> bool:
+                    if child_node is None:
+                        return False
+                    if root_node is child_node:
+                        return True
+                    return _is_child(child_node.parent)
+                if TYPE_CHECKING:
+                    assert isinstance(prop.val, Node)
+                if not _is_child(dep_node):
+                    self._graph.add_edge(root_node, dep_node)
+
     def _process_properties_r(self, root_node: Node, props_node: Node) -> None:
         """
         Process props_node properties for dependencies, and add those as
@@ -2473,12 +2502,16 @@ class EDT:
                         assert isinstance(prop.val, Node)
                     if _is_child(props_node, prop.val):
                         continue
-                self._graph.add_edge(root_node, prop.val)
+                if TYPE_CHECKING:
+                    assert isinstance(prop.val, Node)
+                self._apply_dependency_mode(root_node, prop.val, prop)
             elif prop.type == 'phandles':
                 if TYPE_CHECKING:
                     assert isinstance(prop.val, list)
                 for phandle_node in prop.val:
-                    self._graph.add_edge(root_node, phandle_node)
+                    if TYPE_CHECKING:
+                        assert isinstance(phandle_node, Node)
+                    self._apply_dependency_mode(root_node, phandle_node, prop)
             elif prop.type == 'phandle-array':
                 if TYPE_CHECKING:
                     assert isinstance(prop.val, list)
@@ -2487,7 +2520,7 @@ class EDT:
                         continue
                     if TYPE_CHECKING:
                         assert isinstance(cd, ControllerAndData)
-                    self._graph.add_edge(root_node, cd.controller)
+                    self._apply_dependency_mode(root_node, cd.controller, prop)
 
         # A Node depends on whatever supports the interrupts it
         # generates.
@@ -2993,7 +3026,7 @@ def _bad_overwrite(to_dict: dict, from_dict: dict, prop: str,
         return False
 
     # These are overridden deliberately
-    if prop in {"title", "description", "compatible", "examples"}:
+    if prop in {"title", "description", "compatible", "examples", "dependency-mode"}:
         return False
 
     if prop == "required":
